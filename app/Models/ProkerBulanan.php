6e99e49ff38c6f648fb66cfc3d29db7b38769be5
ahya37 <ahmadyaniid37@gmail.com>
2024-05-29 13:17:41 +0700

    migration proker bulanan

---
+++ app/Models/ProkerBulanan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProkerBulanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'proker_bulanan';
+	protected $guarded = [];
+}
