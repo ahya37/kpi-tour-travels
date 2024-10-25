8addc6232ee872b03013b2928c5428162b58522c
matoi07 <adhitya.dwicahyana86@gmail.com>
2024-10-25 14:11:30 +0700

    Update 25 Oktober 2024 - Penambahan Fitur  > Penambahan Fitur Cronjob tarik absen

---
+++ app/Console/Kernel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+date_default_timezone_set('Asia/Jakarta');
+class Kernel extends ConsoleKernel
+{
+    protected function schedule(Schedule $schedule)
+    {
+        $current_time   = date('H:i');
+
+        if($current_time <= "15:00") {
+            $schedule->command('app:get-data-absen')->everyFiveMinutes();
+        } else {
+            $schedule->command('app:get-data-absen')->everyTwoHours();
+        }
+    }
+
+    protected function commands()
+    {
+        // Daftarkan semua command di siniq
+        $this->load(__DIR__.'/Commands');
+    }
+}
