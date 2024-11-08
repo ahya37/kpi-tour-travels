0d79563f3b663a60a098a693f847417ccfa3d70d
matoi07 <adhitya.dwicahyana86@gmail.com>
2024-11-08 08:50:39 +0700

    Update 08 November 2024 - Modul HR  > Pengajuan Lembur   (-) Fix bug ketika di dashboard ada data, namun ketika membuka modal tidak muncul data apapun

--- public/js/divisi/human_resource/dashboard/index.hr.dashboard.js
+++ public/js/divisi/human_resource/dashboard/index.hr.dashboard.js
@@ -180,7 +180,7 @@ function showModal(idModal, jenis, data)
         let bulanSekarang   = moment(today, 'YYYY-MM-DD').format('MM');
         showSelect('select_pgj_month', dataBulan, bulanSekarang, '');
         // SHOW TABLE
-        showTable('table_pgj_lmb', '');
+        showTable('table_pgj_lmb', bulanSekarang);
     } else if(idModal == 'modal_pgj_lmb_preview') {
 
         // GET DATA PENGAJUAN LEMBUR DETAIL
